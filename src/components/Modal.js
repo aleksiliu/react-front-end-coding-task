733a6f88f7647153a0a1465df7a652ba27cf2693
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-10-26 22:54:43 +0300

    Change modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,10 +5,10 @@ import Button from './Button';
 const Modal = ({ handleClose, show, children }) => {
   return (
     <div className={show ? 'modal show' : 'modal hide'}>
+      <p className="close" onClick={handleClose}>
+        Close
+      </p>
       <section className="modal-main">
-        <p className="close" onClick={handleClose}>
-          Close
-        </p>
         {children}
         <Button variant="secondary" onClick={handleClose}>
           Done
