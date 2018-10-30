ce1b6f18635e57dff15b2b040a87e58580174312
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-10-30 15:27:39 +0200

    Update styles

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './Checkbox.css';
 
-const Checkbox = ({
-  type = 'checkbox',
-  value,
-  isChecked,
-  onChange,
-  children
-}) => (
+const Checkbox = ({ type = 'checkbox', value, isChecked, onChange }) => (
   <input type={type} name="isChecked" checked={isChecked} onChange={onChange} />
 );
 
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Modal.css';
 import Button from './Button';
+import close from '../img/close.svg';
 
 const Modal = ({ handleClose, show, children }) => {
   return (
     <div className={show ? 'modal show' : 'modal hide'}>
-      <p className="close" onClick={handleClose}>
-        Close
-      </p>
+      <img src={close} alt="" className="close" onClick={handleClose} />
       <section className="modal-main">
         {children}
         <Button variant="secondary" onClick={handleClose}>
