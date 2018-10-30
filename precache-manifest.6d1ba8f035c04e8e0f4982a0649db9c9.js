a6ff52d55b734d7a266f8f07ceaf124196717f32
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-10-30 15:30:53 +0200

    Updates

---
+++ precache-manifest.6d1ba8f035c04e8e0f4982a0649db9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11e810738b8f42c12f1e955fc73909d8",
+    "url": "/react-front-end-coding-task/static/media/close.11e81073.svg"
+  },
+  {
+    "revision": "52e28a23f4cd6d44c5cf",
+    "url": "/react-front-end-coding-task/static/js/runtime~main.52e28a23.js"
+  },
+  {
+    "revision": "c4764991743a383fb81c",
+    "url": "/react-front-end-coding-task/static/js/main.c4764991.chunk.js"
+  },
+  {
+    "revision": "6d8b9603c46a7e3c65db",
+    "url": "/react-front-end-coding-task/static/js/1.6d8b9603.chunk.js"
+  },
+  {
+    "revision": "c4764991743a383fb81c",
+    "url": "/react-front-end-coding-task/static/css/main.3b87a6c3.chunk.css"
+  },
+  {
+    "revision": "7f70e579f15f9da42ac92f7c3877cd6b",
+    "url": "/react-front-end-coding-task/index.html"
+  }
+];
